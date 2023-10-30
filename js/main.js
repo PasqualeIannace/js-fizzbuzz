41a85277e555d5e6cc9f0dfed595a7787eb33e1d
PasqualeIannace <pasquale.iannace@gmail.com>
2023-10-30 17:04:25 +0100

    Stampati gli elementi a schermo

--- js/main.js
+++ js/main.js
@@ -1,14 +1,32 @@
+const container = document.getElementById("container")
 
 for (let i = 1; i <= 100; i++) {
 
     
     if (i % 3 == 0 && i % 5 == 0) {
         console.log("SUPER RESTO 3 & 5!", i);
+
+        const fizzBuzz = document.createElement("div");
+        fizzBuzz.innerHTML = `${i} FizzBuz!`;
+        container.append(fizzBuzz);
+
     } else if (i % 3 == 0) {
         console.log("RESTO 3", i);
+
+        const fizz = document.createElement("div");
+        fizz.innerHTML = `${i} Fizz!`;
+        container.append(fizz);
     } else if (i % 5 == 0) {
         console.log("RESTO 5", i);
+
+        const buzz = document.createElement("div");
+        buzz.innerHTML = `${i} Buzz!`;
+        container.append(buzz);
     } else {
         console.log(i);
+
+        const number = document.createElement("div");
+        number.innerHTML = `${i}`;
+        container.append(number);
     }
 }
