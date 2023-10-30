8c8e81fbfd52ef89fefa8d43d9c6bf04abccb346
PasqualeIannace <pasquale.iannace@gmail.com>
2023-10-30 17:22:03 +0100

    Esercizio completato

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ for (let i = 1; i <= 100; i++) {
         const fizzBuzz = document.createElement("div");
         fizzBuzz.innerHTML = `${i} FizzBuz!`;
         container.append(fizzBuzz);
+        fizzBuzz.classList.add("box", "bgRed");
 
     } else if (i % 3 == 0) {
         console.log("RESTO 3", i);
@@ -16,17 +17,20 @@ for (let i = 1; i <= 100; i++) {
         const fizz = document.createElement("div");
         fizz.innerHTML = `${i} Fizz!`;
         container.append(fizz);
+        fizz.classList.add("box", "bgGreen");
     } else if (i % 5 == 0) {
         console.log("RESTO 5", i);
 
         const buzz = document.createElement("div");
         buzz.innerHTML = `${i} Buzz!`;
         container.append(buzz);
+        buzz.classList.add("box", "bgYellow");
     } else {
         console.log(i);
 
         const number = document.createElement("div");
         number.innerHTML = `${i}`;
         container.append(number);
+        number.classList.add("box", "bgBlue");
     }
 }
